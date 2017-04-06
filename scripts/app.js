e3da5934a1b730ec09c490818381a28d89538f3f
sejinpark0209 <sejinpark0209@gmail.com>
2017-04-06 16:18:29 -0700

    move red circle

---
+++ scripts/app.js
@@ -0,0 +1,38 @@
+$(document).on("ready", function(){
+
+	//create function to move red circle
+
+
+		$(document).on('keydown', function(e){
+			if(e.keyCode == 90) {
+				$('#redcirc').animate({
+                    right: '-=10'
+				}, 0);
+			}
+
+		})
+
+		$(document).on('keydown', function(e){
+			if(e.keyCode == 89) {
+				$('#bluecirc').animate({
+                    right: '-=10'
+				}, 0);
+			}
+			
+		})
+
+
+
+		
+	
+});
+
+
+
+
+
+
+
+
+
+
