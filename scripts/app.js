053e0f58393bc51e8b2173906ae73d9916b4749b
sejinpark0209 <sejinpark0209@gmail.com>
2017-04-06 21:19:10 -0700

    pressing each key shows alphabet and keycode but one that after

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,44 @@
 $(document).on("ready", function(){
 
-	//create function to move red circle
+	
 
+		//create function to move red circle
+
+	
+  //   var possible = "abcdefghijklmnopqrstuvwxyz";
+		// var text = "";
+		// 	for( var i=0; i < 5; i++ ) {
+  //       		text += possible.charAt(Math.floor(Math.random() * possible.length));
+  //   		}
+  //   		console.log(text);
+  // keycode. 65 - 90
+ var key = 'x'.charCodeAt(0)-32;
+ console.log(key);
 
 		$(document).on('keydown', function(e){
-			if(e.keyCode == 90) {
-				$('#redcirc').animate({
-                    right: '-=10'
-				}, 0);
+    		var possible = "abcdefghijklmnopqrstuvwxyz";
+			var text = "";
+			for(var i = 0; i < 1; i++ ) {
+        		text += possible.charAt(Math.floor(Math.random() * possible.length));
+    		}
+    		console.log(text);
+    		var splitText = [];
+    		splitText = text.split("");
+
+
+    		for(var i = 0; i < splitText.length; i++) {
+    		
+    			$('.abcBoard1').text(splitText[i]);
+    			var alphaKeyCode = splitText[i].charCodeAt(0)-32;
+    			console.log(alphaKeyCode);
+				if(e.keyCode == alphaKeyCode) {
+					$('#redcirc').animate({
+                   		right: '-=10'
+					}, 0);
+				}
 			}
 
-
-		})
+		});
 
 		$(document).on('keydown', function(e){
 			if(e.keyCode == 88) {
@@ -20,7 +47,7 @@ $(document).on("ready", function(){
 				}, 0);
 			}
 			
-		})
+		});
 
 
 
