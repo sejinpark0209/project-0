346eb3ca708e77aba1d288f862d8232f2df8fc71
sejinpark0209 <sejinpark0209@gmail.com>
2017-04-07 15:13:31 -0700

    added win lost alert with problems

--- scripts/app.js
+++ scripts/app.js
@@ -19,6 +19,44 @@ $(document).on("ready", function(){
 		var text = "";
 		text = possible1.charAt(Math.floor(Math.random() * possible1.length));
 
+		var robotQsn = [
+			{
+				question: "3 + 98",
+				expectedAnswer: "101"
+			},
+			{
+				question: "12 * 12 ?",
+				expectedAnswer: "144"
+			},
+			{
+				question: "6 * 9 ?",
+				expectedAnswer: "54"
+			},
+			{
+				question: "83214 * 39102 ?",
+				expectedAnswer: "3253833828"
+			},
+			{
+				question: "4563452 % 12 ?",
+				expectedAnswer: "8"
+			},
+			{
+				question: "2395123 * 12?",
+				expectedAnswer: "28741476"
+			}
+		]
+
+		
+
+
+		var userAnswer = "";
+		var ranQnum = Math.floor(Math.random() * 6) + 0;
+		console.log(ranQnum);  
+
+
+
+		
+
 		$(document).on('keydown', function(e){
     	
     		var spaceStart = $('.tostart');
@@ -40,12 +78,26 @@ $(document).on("ready", function(){
 
 			var redCircPos = $('#redcirc').position();	
 			console.log(redCircPos);
-			if(redCircPos.left === 400) {
-				alert("player1 won");
-				//reset now
+			if(redCircPos.left === 1000) {
+				if(ranQnum > 2) {
+					userAnswer = prompt(robotQsn[ranQnum].question);
+					if(userAnswer !== robotQsn[ranQnum].question) {
+						alert("Are you robot? you lose!")
+					} else {
+						alert("You are Human! you win!")
+					}
+				} else if (ranQnum < 3) {
+					userAnswer = prompt(robotQsn[ranQnum].question);
+					if(userAnswer !== robotQsn[ranQnum].question) {
+						alert("You are Human! player 1 win!")
+					} else {
+						alert("Are you robot? player 2 win!")
+					}
+				}
 			}
 
 		});
+
 
 		var possible2 = "poilkj"
 		var text2 = "";
@@ -64,20 +116,25 @@ $(document).on("ready", function(){
 			
 			var blueCircPos = $('#bluecirc').position();	
 			console.log(blueCircPos);
-			if(blueCircPos.left === 400) {
-				alert("player2 won");
+			if(blueCircPos.left === 1000) {
+				if(ranQnum > 2) {
+					userAnswer = prompt(robotQsn[ranQnum].question);
+					if(userAnswer !== robotQsn[ranQnum].question) {
+						alert("Are you robot? you lose!")
+					} else {
+						alert("You are Human! you win!")
+					}
+				} else if (ranQnum < 3) {
+					userAnswer = prompt(robotQsn[ranQnum].question);
+					if(userAnswer !== robotQsn[ranQnum].question) {
+						alert("You are Human! you win!")
+					} else {
+						alert("Are you robot? you lose!")
+					}
+				}
 
 			}
-		});
-
-		// var backGround = $('.background');
-		// if($('#redcirc').position() === backGround.offset().right) {
-		// 	console.log("player 1 won");
-		// }
-
-
-
-		
+		});	
 	
 });
 
